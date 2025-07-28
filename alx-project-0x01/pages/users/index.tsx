833e60b8ac028c0e905501815b38d88452662bcc
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-07-29 00:07:03 +0100

    feat: fetch and display users using UserCard component and getStaticProps

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,14 +1,47 @@
-import React from "react";
+import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
 
-
-const UsersPage: React.FC = () => {
+const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
+  console.log(users);
   return (
-    <div className="p-6">
-      <h1 className="text-3xl">Users</h1>
-      <p>This page will list users.</p>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">User</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Edit
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {users?.map(({ username, email, address, geo, phone, website, company }: UserProps, id: number) => (
+            <UserCard
+              id={id}
+              username={username}
+              email={email}
+              address={address}
+              geo={geo}
+              phone={phone}
+              website={website}
+              company={company} name={""}            />
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
 
-export default UsersPage;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  
+  return {
+    props: {
+      users,
+    },
+  };
+}
+
+export default Users;
