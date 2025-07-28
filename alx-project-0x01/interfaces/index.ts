46e1f2e85dac382080e4af9a8e5db0ae321366eb
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-07-29 00:07:27 +0100

    feat: add UserProps interface to define user data structure

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,6 +1,27 @@
+import { Address } from "cluster";
+
 export interface PostProps {
   userId: number;
   id: number;
   title: string;
   body: string;
+}
+
+export interface UserProps { 
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  geo: Geolocation;
+  phone: string;
+  website: string;
+  company: Company;
+
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
