ab9dfd9c57bbf1aaaef26389eb35d1440daeee04
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-07-31 21:15:38 +0100

    feat: fetch and display users using UserCard component and getStaticProps

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,8 +2,8 @@ import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
 import { UserProps } from "@/interfaces";
 
-const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
-  console.log(users);
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  console.log(posts);
   return (
     <div className="flex flex-col h-screen">
       <Header />
@@ -15,30 +15,43 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
           </button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
-          {users?.map(({ username, email, address, geo, phone, website, company, name }: UserProps, id: number) => (
-            <UserCard
-              id={id}
-              username={username}
-              email={email}
-              address={address}
-              geo={geo}
-              phone={phone}
-              website={website}
-              company={company}
-              name={name}
-            />
-          ))}
+          {posts?.map(
+            (
+              {
+                username,
+                email,
+                address,
+                geo,
+                phone,
+                website,
+                company,
+                name,
+              }: UserProps,
+              id: number
+            ) => (
+              <UserCard
+                id={id}
+                username={username}
+                email={email}
+                address={address}
+                geo={geo}
+                phone={phone}
+                website={website}
+                company={company}
+                name={name}
+              />
+            )
+          )}
         </div>
       </main>
     </div>
   );
 };
 
-
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await response.json();
-  
+
   return {
     props: {
       users,
