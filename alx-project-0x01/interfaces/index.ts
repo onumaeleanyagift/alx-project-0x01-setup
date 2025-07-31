d95d7abbbc3dd2f4859b9c5fdc9f119fbf69c81b
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-07-31 23:02:10 +0100

    feat: add PostModal component and integrate post creation functionality

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -38,3 +38,14 @@ export interface Company {
   bs: string;
 }
 
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,8 +1,19 @@
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostData, PostProps } from "@/interfaces";
+import { useState } from "react";
 
 const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
+  const [isModelOpen, setModalOpen] = useState(false);
+  const [post, stePost] = useState<PostData | null>
+    (null);
+  
+  const handleAddPost = (newPost: PostData) => {
+    stePost({ ...newPost, id: posts.length + 1 });
+  };
+
+
   console.log(posts);
   return (
     <div className="flex flex-col h-screen">
@@ -10,7 +21,7 @@ const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
       <main className="p-4">
         <div className="flex justify-between">
           <h1 className=" text-2xl font-semibold">Post Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+          <button onClick={() => setModalOpen(true)} className="bg-blue-700 px-4 py-2 rounded-full text-white">
             Add Post
           </button>
         </div>
@@ -26,6 +37,11 @@ const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
           ))}
         </div>
       </main>
+
+      {isModelOpen && (
+        <PostModal onClose={() => setModalOpen(false)}
+        onSubmit={handleAddPost} />
+      )}
     </div>
   );
 };
